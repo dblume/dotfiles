f63806f2bab0da543e4dc2244b06ee87fec4caf2
David Blume <david.blume@gmail.com>
2020-06-23 13:36:06 -0700

    Add .tmux.conf to list of files to update.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ set -eu -o pipefail # See: https://sipb.mit.edu/doc/safe-shell/
 
 declare -r SCRIPT_NAME=$(basename "$BASH_SOURCE")
 declare -r backup_dir=$HOME/backup_of_dotfiles_`date "+%Y-%m-%d"`
-declare -a dotfiles=(".bashrc" ".bash_profile" ".vimrc" ".editrc"
-                     ".gitconfig" ".gitignore" ".inputrc" ".ssh/config")
+declare -a dotfiles=(".bashrc" ".bash_profile" ".vimrc" ".editrc" ".gitconfig"
+                     ".gitignore" ".inputrc" ".tmux.conf" ".ssh/config")
 declare -i DRY_RUN=0
 
 ## exit the shell (with status 2) after printing the message
